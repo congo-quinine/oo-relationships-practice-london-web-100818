34778727b57a1b4a5294001b7473bef412874381
congo-quinine <congo.quinine@gmail.com>
2018-10-15 19:02:36 +0100

    finitto

--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,21 @@ def reload
   load 'config/environment.rb'
 end
 
+house_london = Listing.new('Small house' , 'London')
+house_japan = Listing.new('Big House', 'Japan' )
+rob = Guest.new('Rob')
+ash = Guest.new('Ash')
+pika = Guest.new('Pika')
+rob = Guest.new('Rob')
+
+new_trip1 = Trip.new(house_london, rob)
+new_trip2 = Trip.new(house_london, ash)
+new_trip3 = Trip.new(house_japan, pika)
+new_trip5 = Trip.new(house_japan, rob)
+new_trip6 = Trip.new(house_japan, rob)
+
+# rob.new_trip(house_london)
+# ash.new_trip(house_london)
+# pika.new_trip(house_london)
+
 Pry.start
